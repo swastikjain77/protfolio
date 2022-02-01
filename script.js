0aea7e461072f86bfb70fa96e6d48d34361a1239
swastikjain77 <59052688+swastikjain77@users.noreply.github.com>
2022-02-01 15:37:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -27,13 +27,13 @@ $(document).ready(function() {
 
     // typing animaton script 
     var typed = new Typed(".typing", {
-        strings: ["YouTuber", "Developer", "Blogger", "Desinger", "Freelancer"],
+        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
     var typed = new Typed(".typing-2", {
-        strings: ["YouTuber", "Developer", "Blogger", "Desinger", "Freelancer"],
+        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
